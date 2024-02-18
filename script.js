be6b8863200d974757b0a0b23f242d9046e1a859
Thalles Sato <67062538+ThallesSato@users.noreply.github.com>
2024-02-18 13:29:44 -0300

    Update prices

--- script.js
+++ script.js
@@ -147,12 +147,12 @@ $(document).ready(function() {
 
     // fechartd();
 
-     setTimeout(function() {
-       alert("Todos os valores devem ser consultados devido as festas, agradecemos a compreensão.");
-     }, 1000);
-
-    //setTimeout(function() {
-    //   alert("Nosso cardápio ainda está em desenvolvimento, agradecemos a compreensão");
+    // setTimeout(function() {
+    //   alert("Todos os valores devem ser consultados devido as festas, agradecemos a compreensão.");
     // }, 1000);
+
+    setTimeout(function() {
+       alert("Nosso cardápio ainda está em desenvolvimento, agradecemos a compreensão");
+    }, 1000);
     
   });
